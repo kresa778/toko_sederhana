258c247f6e6a41c7f79b80734d4930d761097492
kresa usman <kresapirdaus77@gmail.com>
2025-07-26 15:42:45 +0700

    menambahkan seluruh laravel

---
+++ database/migrations/2025_07_26_072047_add_pelanggan_to_pelanggan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pelanggan', function (Blueprint $table) {
+            $table->bigIncrements('id'); //primary key
+            $table->string('nama_lengkap');
+            $table->string('email')->unique();
+            $table->string('no_hp')->nullable();
+            $table->text('alamat')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_custom');
+    }
+};
---
+++ database/migrations/2025_07_26_072133_add_transaksi_to_transaksi_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transaksi', function (Blueprint $table) {
+            $table->bigIncrements('id'); // Primary key
+            $table->date('tanggal_transaksi');
+            $table->integer('jumlah');
+            $table->decimal('total_harga', 10, 2);
+            $table->timestamps();
+
+            //Relations
+            $table->foreignId('pelanggan_id')->constrained('pelanggan')->onDelete('cascade');
+            $table->foreignId('produk_id')->constrained('produk')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
